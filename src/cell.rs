4beace3582e92ab57719c9a12be1a7b9020ea91b
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2024-08-02 21:26:04 +0200

    Add intial living status for cell

--- src/cell.rs
+++ src/cell.rs
@@ -1,13 +1,25 @@
+use rand::Rng;
+
 pub struct Cell {
-    pub alive: bool,
+    alive: bool,
 }
 
 impl Cell {
+    pub fn new(living_chance: f64) -> Self {
+        Self {
+            alive: rand::thread_rng().gen_bool(living_chance),
+        }
+    }
+
     pub fn to_string(&self) -> String {
         if self.alive {
             String::from("x")
         } else {
-            String::from(" ")
+            String::from(".")
         }
+    }
+
+    pub fn update_alive_status(&mut self) {
+        self.alive = !self.alive;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,17 @@ mod cell;
 use crate::cell::Cell;
 
 fn main() {
-    let my_cell: Cell = Cell { alive: false };
+    let living_chance: f64 = 0.33;
+
+    let mut my_cell: Cell = Cell::new(living_chance);
+
+    println!("{}", my_cell.to_string());
+
+    my_cell.update_alive_status();
+
+    println!("{}", my_cell.to_string());
+
+    my_cell.update_alive_status();
 
     println!("{}", my_cell.to_string());
 }
