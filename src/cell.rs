30528240d154e6cb9727fc8d25ed59fa9dd0237f
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2024-08-03 20:34:07 +0200

    Add living logic to cell

--- src/cell.rs
+++ src/cell.rs
@@ -2,12 +2,14 @@ use rand::Rng;
 
 pub struct Cell {
     alive: bool,
+    living_neighbor_count: u8,
 }
 
 impl Cell {
     pub fn new(living_chance: f64) -> Self {
         Self {
             alive: rand::thread_rng().gen_bool(living_chance),
+            living_neighbor_count: 0,
         }
     }
 
@@ -19,7 +21,16 @@ impl Cell {
         }
     }
 
+    pub fn update_living_neighbor_count(&mut self, neighbors: Vec<&Cell>) {
+        self.living_neighbor_count = neighbors.iter().filter(|cell| cell.alive).count() as u8
+    }
+
     pub fn update_alive_status(&mut self) {
-        self.alive = !self.alive;
+        match self.living_neighbor_count {
+            0 | 1 => self.alive = false,
+            2 => self.alive = self.alive,
+            3 => self.alive = true,
+            4.. => self.alive = false,
+        }
     }
 }
